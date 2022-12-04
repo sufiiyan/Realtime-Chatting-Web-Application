77d2d9d2544d5fe161a4f2153a944acce88e4578
Sufiyan Pasha <97835883+sufiiyan@users.noreply.github.com>
2022-12-04 21:08:30 +0530

    Add files via upload

---
+++ client/src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
+
+function Home() {
+    return (
+        <Row>
+            <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+                <div className="main-div">
+                    <h1 style={{"margin-bottom":"15px"}}>Share the world with your friends</h1>
+                    <p style={{"margin-bottom":"50px"}}>Chat App lets you connect with the world</p>
+                    <LinkContainer to="/chat">
+                        <Button variant="success">
+                            Get Started <i className="fas fa-comments home-message-icon"></i>
+                        </Button>
+                    </LinkContainer>
+                </div>
+            </Col>
+            <Col md={6} className="home__bg"></Col>
+        </Row>
+    );
+}
+
+export default Home;
